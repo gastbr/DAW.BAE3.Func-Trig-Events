36c616922366e780362dc655ababd8c51f8187c2
Alumgs <alumgs@majadamarcial.es>
2024-05-28 17:51:15 +0100

    first commit

---
+++ triggers.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+USE test;
+
+-- Paso 2 
+CREATE TABLE alumnos (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido1 VARCHAR(50) NOT NULL,
+    apellido2 VARCHAR(50), 
+    nota FLOAT,
+    email char(30)
+);
+
+-- Paso 3
+DELIMITER $$
+DROP TRIGGER IF EXISTS trigger_check_nota_before_insert$$
+CREATE TRIGGER trigger_check_nota_before_insert
+BEFORE INSERT
+ON alumnos FOR EACH ROW
+BEGIN
+  IF NEW.nota < 0 THEN
+    set NEW.nota = 0;
+  ELSEIF NEW.nota > 10 THEN
+    set NEW.nota = 10;
+  END IF;
+END$$
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS trigger_check_nota_before_update$$
+CREATE TRIGGER trigger_check_nota_before_update
+BEFORE UPDATE
+ON alumnos FOR EACH ROW
+BEGIN
+  IF NEW.nota < 0 THEN
+    set NEW.nota = 0;
+  ELSEIF NEW.nota > 10 THEN
+    set NEW.nota = 10;
+  END IF;
+END$$
+
+-- Paso 4
+DELIMITER ;
+INSERT INTO alumnos VALUES (1, 'Pepe', 'López', 'López', -1);
+INSERT INTO alumnos VALUES (2, 'María', 'Sánchez', 'Sánchez', 11);
+INSERT INTO alumnos VALUES (3, 'Juan', 'Pérez', 'Pérez', 8.5);
+
+-- Paso 5
+SELECT * FROM alumnos;
+
+-- Paso 6
+UPDATE alumnos SET nota = -4 WHERE id = 3;
+UPDATE alumnos SET nota = 14 WHERE id = 3;
+UPDATE alumnos SET nota = 9.5 WHERE id = 3;
+
+-- Paso 7
+SELECT * FROM alumnos;
+
+
